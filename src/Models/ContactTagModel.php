94cc9a8ecde5da7b54f47c458d2acf584f1fda38
Lonnie Ezell <lonnieje@gmail.com>
2026-05-19 09:29:11 +0200

    style: remove auto-generated license file headers

--- src/Models/ContactTagModel.php
+++ src/Models/ContactTagModel.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * This file is part of YourVendor/YourPackage.
- *
- * (c) Your Name <you@example.com>
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
 namespace Myth\Courier\Models;
 
 use CodeIgniter\Model;
